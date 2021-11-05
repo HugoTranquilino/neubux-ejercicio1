56e00a5ddcb109a924687b0048c1125f10fed4dd
Hugo <hugotranquilino@gmail.com>
2021-11-05 15:54:59 -0600

    estilizacion de la interfaz

--- index.php
+++ index.php
@@ -69,6 +69,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Neubux ejercicio 1</title>
+
+    <!-- style -->
+    <link rel="stylesheet" href="main.css">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
     
